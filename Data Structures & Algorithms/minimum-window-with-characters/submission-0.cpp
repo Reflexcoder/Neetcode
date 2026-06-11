0ff217bd11848725b67562f10211e232e8184842
Mayur pal <98248896+Reflexcoder@users.noreply.github.com>
2026-06-11 15:10:11 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        unordered_map<char,int>map1,map2;
+        for(int i=0;i<m;i++){
+            map2[t[i]]++;
+        }
+        int left=0;
+        int min_len=0;
+        int formed=0;
+        int needed=map2.size();
+        int start=0;
+        for(int right=0;right<n;right++){
+               map1[s[right]]++;
+               if(map2.find(s[right])!=map2.end() && map1[s[right]]==map2[s[right]]){
+                   formed++;
+               
+               }
+        
+        while(formed==needed){
+            if(min_len==0 || min_len>right-left+1){
+                min_len=right-left+1;
+                start=left;
+            }
+            map1[s[left]]--;
+            if(map2.find(s[left])!=map2.end() && map1[s[left]]<map2[s[left]]){
+                 formed--;
+            }
+            left++;
+
+        }
+               
+        }
+        if(min_len==0){
+            return "";
+        }
+        return s.substr(start,min_len);
+    }
+};
