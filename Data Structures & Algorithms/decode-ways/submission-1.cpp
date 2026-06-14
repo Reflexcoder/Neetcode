13c58fceefcd9bf02481a8fcc92779571d3c7d84
Mayur pal <98248896+Reflexcoder@users.noreply.github.com>
2026-06-14 13:38:43 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(string &s, int index){
+        int n=s.size();
+      if(index==n){
+        return 1;
+      }
+      if(s[index]=='0'){
+        return 0;
+      }
+      int ways=0;
+      ways+=solve(s,index+1);
+      if(index+1<n){
+        int two=(s[index]-'0')*10 + (s[index+1]-'0');
+        if(two>=10 && two<=26){
+            ways+=solve(s,index+2);
+        }
+      }
+      return ways;
+
+    }
+    int numDecodings(string s) {
+       return solve(s,0);
+    }
+};
