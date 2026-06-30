0c1900237fc36424797568c7b253323bfd7384df
Mayur pal <98248896+Reflexcoder@users.noreply.github.com>
2026-06-30 21:41:39 +0530

    Add: triangle - submission-7

---
+++ Data Structures & Algorithms/triangle/submission-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int solve(vector<vector<int>>& triangle,int row, int col,vector<vector<int>>&dp){
+    int n=triangle.size();
+    
+         if(row==n){
+            return 0;
+         }
+         if(dp[row][col]!=INT_MIN){
+             return dp[row][col];
+         }
+         int takefirst=triangle[row][col]+solve(triangle,row+1,col,dp);
+         int takesecond=triangle[row][col]+solve(triangle,row+1,col+1,dp);
+         return dp[row][col]=min(takefirst,takesecond);
+
+}
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        
+        vector<vector<int>>dp(n+1,vector<int>(n+1,INT_MIN));
+       int ans= solve(triangle, 0,0,dp);
+       return ans;
+    }
+};
