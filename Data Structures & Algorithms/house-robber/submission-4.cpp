50eb6a5434a78c2919844fefa139c8ae43c6a94e
Mayur pal <98248896+Reflexcoder@users.noreply.github.com>
2026-07-21 16:47:54 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int solve(vector<int>& nums,int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return nums[0];
+    }
+    int prev2=0;
+    int prev1=nums[0];
+    for(int i=2;i<=n;i++){
+        int current=max(nums[i-1] + prev2,prev1 );
+        prev2=prev1;
+        prev1=current;
+    }
+    return prev1;
+}
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        
+        int ans=solve(nums, n);
+        return ans;
+    }
+};
