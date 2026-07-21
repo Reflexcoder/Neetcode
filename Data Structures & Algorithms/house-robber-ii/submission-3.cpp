ac3af238c7b91e324c3025978e18139bef01fbae
Mayur pal <98248896+Reflexcoder@users.noreply.github.com>
2026-07-21 16:32:41 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int solve(vector<int>&nums,int start,int end){
+        if(end<start){
+            return 0;
+        }
+        if(end==start){
+            return nums[start];
+        }
+         int n=end-start+1;
+        vector<int>dp(n+1,0);
+        dp[0]=0;
+        dp[1]=nums[start];
+       
+        for(int i=2;i<=n;i++){
+            dp[i]=max(nums[start+i-1] + dp[i-2],dp[i-1]);
+        }
+        return dp[end];
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return nums[0];
+        }
+        
+        int houseA=solve(nums,0,n-2);
+        int houseB=solve(nums,1,n-1);
+        return max(houseA,houseB);
+    }
+};
