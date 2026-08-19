1b176df408871f87737e6d723cdef884954ba139
Mayur pal <98248896+Reflexcoder@users.noreply.github.com>
2026-08-19 12:19:20 +0530

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+int solve(int m,int n,int row,int col, vector<vector<int>>&dp){
+  if(row<0 || row >=m || col<0 || col>=n){
+    return 0;
+  }
+  if(row==m-1 && col==n-1){
+    return 1;
+  }
+
+    int take_right=solve(m,n,row,col+1,dp);
+    int take_down=solve(m,n,row+1,col,dp);
+
+    return take_right + take_down;
+
+}
+    int uniquePaths(int m, int n) {
+      vector<vector<int>>dp(m,vector<int>(n,-1));
+        int ans=solve(m,n,0,0,dp);
+        return ans;
+    }
+};
