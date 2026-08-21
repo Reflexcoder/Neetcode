b995d21eefd6a7102565695eff12cbc142054214
Mayur pal <98248896+Reflexcoder@users.noreply.github.com>
2026-08-21 13:18:14 +0530

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+int solve(vector<int>&nums,int remAmount,int index){
+  if(remAmount==0){
+    return 1;
+  }
+  if(remAmount<0 || remAmount>0 || index>=nums.size()){
+    return 0;
+  }
+  int take_plus=solve(nums,remAmount-nums[index],index+1);
+  int take_minus=solve(nums,remAmount+nums[index],index+1);
+
+  return take_plus + take_minus;
+}
+    int findTargetSumWays(vector<int>& nums, int target) {
+       return solve(nums,target,0);
+    }
+};
