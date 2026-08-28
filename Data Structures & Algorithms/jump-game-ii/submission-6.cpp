d6e38c6275d6e5f0e28b1a671e6f7ec98644c3fa
Mayur pal <98248896+Reflexcoder@users.noreply.github.com>
2026-08-28 13:17:31 +0530

    Add: jump-game-ii - submission-6

---
+++ Data Structures & Algorithms/jump-game-ii/submission-6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int max_reach=0;
+        int local_reach=0;
+        int jump=0;
+        for(int i=0;i<n;i++){
+            max_reach=max(max_reach,nums[i] + i);
+            if(i==local_reach){
+                jump++;
+                local_reach=max_reach;
+            }
+            if(local_reach>=n){
+                break;
+            }
+        }
+        return jump;
+    }
+};
